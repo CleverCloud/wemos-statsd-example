879832a7f2c2c761556b34a75a3d87b76ea79410
Clément Nivolle <clement@nivolle.com>
2018-04-06 19:31:25 +0200

    server update

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const server = require("http").Server(app);
 const WebSocket = require("ws")
 const bodyParser = require("body-parser");
+var path = require("path");
 
 server.listen(process.env.PORT || 8080);
+
+app.use(express.static(__dirname + '/public'));
+
 const wsServer = new WebSocket.Server({ server });
 
 app.use(bodyParser.text());
 
+
+
 app.get("/", function(req, res) {
-  res.send("Hello.");
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.put("/color", function(req, res) {
@@ -19,6 +26,15 @@ app.put("/color", function(req, res) {
     }
   });
   res.send("Ok.");
+});
+
+app.get("/status", function (req, res) {
+  var i = 0;
+  wsServer.clients.forEach(function each(client) {
+    i++;
+  });
+  res.send(JSON.stringify({client_number:i}));
+
 });
 
 wsServer.on("connection", function (ws, req) {
